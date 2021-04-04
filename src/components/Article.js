c6e262840ff644a36a00b4f666676fe3cbb7bdad
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-04-04 23:01:13 +0100

    link blank

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,11 @@ import React from "react";
 const Article = (props) => {
   return (
     <article>
-      <a href={"https://reddit.com" + props.article.permalink} target="_blank">
+      <a
+        href={"https://reddit.com" + props.article.permalink}
+        target="_blank"
+        rel="noreferrer"
+      >
         <h3>{props.article.title}</h3>
       </a>
     </article>
